3bb99051c8397106c213a9910a79976aec826b04
ravczar <ravczar@gmail.com>
2020-03-21 21:59:32 +0100

    Lab2 commit, created 4 views (main, bmi, bwr, recipes).

---
+++ PPM/app/src/main/java/com/pjatk/s16281/PPM.java
@@ -0,0 +1,14 @@
+package com.pjatk.s16281;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PPM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ppm);
+    }
+}
